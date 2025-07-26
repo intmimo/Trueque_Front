71d6ac31c36cbcfe94e817a4a53033b27d1c91a8
Yael <jperezyael@gmail.com>
2025-07-25 20:40:48 -0600

    Uniendo back de registro y login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,39 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private API_URL = 'http://localhost:8000/api'; // Asegúrate que es tu URL correcta
 
-  private usuarios: any[] = [];
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
 
-  //funcion de la pagina de registro de usuario
-  register(nombre: string, email: string, password: string, colonia: string, municipio: string): void {
-  const user = { nombre, email, password, colonia, municipio };
-  const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
-  usuarios.push(user);
-  localStorage.setItem('usuarios', JSON.stringify(usuarios));
-}
+
+    //funcion de la pagina de registro de usuario
+  register(nombre: string, email: string, password: string, colonia: string, municipio: string): Observable<any> {
+    return this.http.post(`${this.API_URL}/register`, {
+      name: nombre,
+      email,
+      password,
+      password_confirmation: password, // <- IMPORTANTE
+      colonia,
+      municipio
+    });
+  }
 
   //funcion de la pagina de inicio de sesion
-  login(email: string, password: string): boolean {
-    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
-    const user = usuarios.find((u: any) => u.email === email && u.password === password);
-    if (user) {
-      localStorage.setItem('logueado', JSON.stringify(user));
-      return true;
-    }
-    return false;
-  }
-
-  //muestra que el usuario esta deslogueadao en el localstorage
-  logout() {
-    localStorage.removeItem('logueado');
-  }
-  //muestra que el usuario esta logueadao en el localstorage
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem('logueado');
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.API_URL}/login`, {
+      email,
+      password
+    });
   }
 }
