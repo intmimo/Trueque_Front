462272572d44b4f1cce5467eb3c8dbe33ba2fa3f
intmimito01 <noemiarango6@gmail.com>
2025-08-09 13:56:02 -0600

    Elimine una linea duplicada en el module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,6 @@ import { RateUserModalComponent } from './components/rate-user-modal/rate-user-m
     FormsModule,      // 📌 NECESARIO PARA [(ngModel)]
     CommonModule      // 📌 NECESARIO PARA directivas *ngIf, *ngFor, etc.
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
